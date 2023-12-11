b590feb307fd805170b5937bcc3c1abc6d52bf6f
Dhruvilshah098 <52992665+Dhruvilshah098@users.noreply.github.com>
2023-12-11 23:08:02 +0530

    Inital code setup

---
+++ YUJCSR/Controllers/SampleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YUJCSR.Controllers
+{
+    public class SampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        public IActionResult Messages()
+        {
+            return View();
+        }
+
+        public IActionResult Settings()
+        {
+            return View();
+        }
+    }
+}
